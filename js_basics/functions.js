8b77228156dc80e161387be33fe770eb45064084
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2025-08-28 10:24:30 +0530

    Add files via upload

---
+++ js_basics/functions.js
@@ -0,0 +1,13 @@
+//function declaration
+function add(a, b) {
+  return a + b;
+}
+console.log(add(2, 3)); 
+
+// function expression
+const multiply = function(a,b){
+    return a*b;
+}
+
+// arrow function
+const greet = (name) => `Hello, ${name}!`;
