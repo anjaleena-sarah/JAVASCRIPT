ba24aa2861b7907e5dceb79a78c9dac9fa97bdc5
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2025-08-29 09:13:39 +0530

    Add files via upload

---
+++ js_basics/name.js
@@ -0,0 +1,18 @@
+// console.log is used to display messages
+console.log('Hello World');
+
+// let is used to declare a variable in JS
+let name = 'ASKR';
+// by default variables that we defined in JavaScript their value is undefined now we can optionally initialize this variable
+console.log(name);
+
+// RULES FOR VARIABLE DECLARATION
+// Cannot be a reserved keyword
+// Should be meaningful - use descriptive names
+// Cannot start with a number
+// Cannot contain a space or hypen(-) - use camelCase
+// They are case sentitive
+
+let firstName='Anjaleena';
+let lastName='Sarah';
+console.log(firstName,lastName);
