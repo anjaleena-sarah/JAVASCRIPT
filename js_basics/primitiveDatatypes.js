9767b6eca221077a9a5e722c60a54add3f5b9f30
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2025-09-08 12:20:23 +0530

    Add files via upload

---
+++ js_basics/primitiveDatatypes.js
@@ -0,0 +1,8 @@
+let name = 'Anjaleena'; //String Literal
+let age = 30; //Number Literal
+let isApproved = true; // or false Boolean Literal
+let firstName = undefined; //undefined
+let lastName = null; //null is used to explicitly clear the value of the variable
+let sym = Symbol("id"); //Symbol - Used to create unique identifiers
+let bigNum = BigInt(900878987654362727); //BIGINT - used for large numbers
+console.log(typeof(bigNum));
